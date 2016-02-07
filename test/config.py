9789917992ab1791fb98e4e22f1eecb43e2f5aa8
Volker Braun <vbraun.name@gmail.com>
2016-02-07 16:05:56 +0100

    Initial commit

---
+++ test/config.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Test Configuration
+"""
+
+#*****************************************************************************
+#       Copyright (C) 2016 Volker Braun <vbraun.name@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#                  http://www.gnu.org/licenses/
+#*****************************************************************************
+
+import os
+
+
+NO_INTERNET = ('NO_INTERNET' in os.environ)
